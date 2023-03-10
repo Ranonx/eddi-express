0edc721aa7f3c21fa8a4617b6b31fd6128e293a2
Ranonx <108208227+Ranonx@users.noreply.github.com>
2023-03-10 10:24:02 +0800

    Update index.js
    added text-message template

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const { init: initDB, Counter } = require("./db");
+const bodyParser = require("body-parser"); // added 10-03-22
+const request = require("request");
 
 const logger = morgan("tiny");
 
@@ -11,6 +13,35 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 app.use(logger);
+
+//////////////////////// added 10-03-22
+
+app.use(bodyParser.raw());
+app.use(bodyParser.json({}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.all("/", async (req, res) => {
+  console.log("消息推送", req.body);
+  const { ToUserName, FromUserName, MsgType, Content, CreateTime } = req.body;
+  if (MsgType === "text") {
+    if (Content === "回复文字") {
+      res.send({
+        ToUserName: FromUserName,
+        FromUserName: ToUserName,
+        CreateTime: CreateTime,
+        MsgType: "text",
+        Content: "这是回复的消息",
+      });
+    }
+  } else {
+    res.send("success");
+  }
+});
+
+// app.listen(PORT, function () {
+//   console.log(`运行成功，端口：${PORT}`)
+// })
+////////////////////////
 
 // 首页
 app.get("/", async (req, res) => {
@@ -59,3 +90,24 @@ async function bootstrap() {
 }
 
 bootstrap();
+
+function sendmess(appid, mess) {
+  return new Promise((resolve, reject) => {
+    request(
+      {
+        method: "POST",
+        url: `http://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=${appid}`,
+        body: JSON.stringify(mess),
+      },
+      function (error, response) {
+        if (error) {
+          console.log("接口返回错误", error);
+          reject(error.toString());
+        } else {
+          console.log("接口返回内容", response.body);
+          resolve(response.body);
+        }
+      }
+    );
+  });
+}
