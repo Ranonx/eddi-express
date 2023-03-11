2c5faa419e22064bca49f14eddf7b96d4fe11c7a
Ranonx <108208227+Ranonx@users.noreply.github.com>
2023-03-11 18:10:26 +0800

    update for variables dob and name

--- index.js
+++ index.js
@@ -12,6 +12,29 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
+const COS = require("cos-nodejs-sdk-v5");
+const cos = new COS({
+  SecretId: "AKIDb3IK7g095nFUUGn6CLFtITP7brsd7NwF",
+  SecretKey: "6xay6ybz7zdYEfqzCCyWrxrb9SYSt76q",
+});
+
+const birthDate = "19931020";
+const userName = "CHOW";
+
+const params = {
+  Bucket: "file-storage-1312367695",
+  Region: "ap-nanjing",
+  Key: `${birthDate}_${userName}_051633_000736_Report.pdf`,
+};
+
+cos.getObjectUrl(params, (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log("File URL:", data.Url);
+  }
+});
+
 app.all("/", async (req, res) => {
   console.log("消息推送", req.body);
   // 从 header 中取appid，如果 from-appid 不存在，则不是资源复用场景，可以直接传空字符串，使用环境所属账号发起云调用
@@ -19,22 +42,28 @@ app.all("/", async (req, res) => {
   const { ToUserName, FromUserName, MsgType, Content, CreateTime } = req.body;
   console.log("推送接收的账号", ToUserName, "创建时间", CreateTime);
   if (MsgType === "text") {
-    if (Content === "我的报告") {
+    if (Content === `${birthDate}_${userName}`) {
       // 小程序、公众号可用
       try {
-        const result = await sendmess(appid, {
-          touser: FromUserName,
-          msgtype: "text",
-          text: {
-            content:
-              "https://file-storage-1312367695.cos.ap-nanjing.myqcloud.com/example.pdf",
-          },
+        const fileParams = {
+          Bucket: "file-storage-1312367695",
+          Region: "ap-nanjing",
+          Key: `${birthDate}_${userName}_051633_000736_Report.pdf`,
+        };
+        cos.headObject(fileParams, (err, data) => {
+          if (err) {
+            console.error(err);
+            res.send("no file exists");
+          } else {
+            console.log("File exists");
+            res.send({
+              downloadLink: `https://file-storage-1312367695.cos.ap-nanjing.myqcloud.com/${birthDate}_${userName}_051633_000736_Report.pdf`,
+            });
+          }
         });
-        console.log("发送消息成功", result);
-        res.send("success");
       } catch (error) {
-        console.log("发送消息失败", error);
-        res.status(500).send("Failed to send message.");
+        console.log("获取文件失败", error);
+        res.status(500).send("Failed to get file.");
       }
     } else {
       res.send("success");
@@ -54,25 +83,3 @@ async function bootstrap() {
 }
 
 bootstrap();
-
-function sendmess(appid, mess) {
-  return new Promise((resolve, reject) => {
-    const request = require("request");
-    request(
-      {
-        method: "POST",
-        url: `http://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=${appid}`,
-        body: JSON.stringify(mess),
-      },
-      function (error, response) {
-        if (error) {
-          console.log("接口返回错误", error);
-          reject(error.toString());
-        } else {
-          console.log("接口返回内容", response.body);
-          resolve(response.body);
-        }
-      }
-    );
-  });
-}
