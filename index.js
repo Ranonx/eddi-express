c43a31147aeeacd71641e179ad356da58ad7d44b
Ranonx <108208227+Ranonx@users.noreply.github.com>
2023-03-12 08:38:13 +0800

    return to safe version with cleanup & comments

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const { init: initDB } = require("./db");
-
 const logger = require("morgan")("tiny");
-
 const app = express();
 
 // Middleware
@@ -13,20 +10,26 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
-// Function to download file from cloud storage service
-function downloadFile(birthDate, userName) {
-  const fileId = `cloud://prod-8gj9vt8j4e3adc47.7072-prod-8gj9vt8j4e3adc47-1317188113/${birthDate}_${userName}.pdf`;
-
-  return cloud
-    .downloadFile({ fileID: fileId })
-    .then((res) => {
-      console.log(`File downloaded successfully: ${res.tempFilePath}`);
-      return res.tempFilePath;
-    })
-    .catch((err) => {
-      console.error(`Error downloading file: ${err.message}`);
-      throw err;
-    });
+function sendmess(appid, mess) {
+  return new Promise((resolve, reject) => {
+    const request = require("request");
+    request(
+      {
+        method: "POST",
+        url: `http://api.weixin.qq.com/cgi-bin/message/custom/send?from_appid=${appid}`,
+        body: JSON.stringify(mess),
+      },
+      function (error, response) {
+        if (error) {
+          console.log("接口返回错误", error);
+          reject(error.toString());
+        } else {
+          console.log("接口返回内容", response.body);
+          resolve(response.body);
+        }
+      }
+    );
+  });
 }
 
 // Route to handle incoming messages
@@ -36,41 +39,29 @@ app.all("/", async (req, res) => {
   const appid = req.headers["x-wx-from-appid"] || "";
   const { ToUserName, FromUserName, MsgType, Content, CreateTime } = req.body;
   console.log("推送接收的账号", ToUserName, "创建时间", CreateTime);
-
   if (MsgType === "text") {
-    // Extract birth date and username from message content
-    const pattern = /^(\d{4}-\d{2}-\d{2})_(\w+)$/;
-    const match = Content.match(pattern);
-
-    if (match) {
-      const birthDate = match[1];
-      const userName = match[2];
-
-      // Validate input
-      if (!birthDate || !userName) {
-        console.error("Invalid birth date or username");
-        res.status(400).json({ message: "Invalid input" });
-        return;
-      }
-
-      // Download file
-      downloadFile(birthDate, userName)
-        .then((tempFilePath) => {
-          // Handle downloaded file
-          res.status(200).json({ message: "File downloaded successfully" });
-        })
-        .catch((err) => {
-          // Handle errors
-          console.error("Error downloading file", err);
-          res.status(500).json({ message: "Error downloading file" });
+    if (Content === "我的报告") {
+      // 小程序、公众号可用
+      try {
+        const result = await sendmess(appid, {
+          touser: FromUserName,
+          msgtype: "text",
+          text: {
+            content:
+              "https://file-storage-1312367695.cos.ap-nanjing.myqcloud.com/example.pdf",
+          },
         });
+        console.log("发送消息成功", result);
+        res.send("success");
+      } catch (error) {
+        console.log("发送消息失败", error);
+        res.status(500).send("Failed to send message.");
+      }
     } else {
-      // Invalid message content
-      res.status(400).json({ message: "Invalid input" });
+      res.send("success");
     }
   } else {
-    // Unsupported message type
-    res.status(400).json({ message: "Unsupported message type" });
+    res.send("success");
   }
 });
 
